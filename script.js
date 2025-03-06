b0a836608097d168733a1bb3069bdfd6fd9d7624
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-06 13:07:56 +0100

    v3 start :  done  :art:

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const input = document.querySelector('input');
 const h2 = document.querySelector('h2');
 
+h2.innerHTML = localStorage.getItem('value');
+
 input.addEventListener('keyup', display);
 
 function display() {
-    h2.innerHTML = input.value;
+    localStorage.setItem('value', input.value);
+    h2.innerHTML = localStorage.getItem('value');
 }
